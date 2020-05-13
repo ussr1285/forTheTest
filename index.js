43718f9cc40f3a77f7a7ffea33f9860ef8cca2dc
Minjae Cho <ussr1285@gmail.com>
2020-05-13 17:51:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 (function () {
 
-    // react-native에 통신을 요청한다.
-    $('#btnSend').on('click', function() {
-        var option = {
-            url: '/movie.json'
-        }
+    // react-native에 통신을 요청한다.   
+     $('#locationSend').on('click', function() {
 
         // "getMovieList"는 react-native에서 받는 메서드 이름입니다.
-        window.webViewBridge.send('getMovieList', option, function(res) {
+        window.webViewBridge.locationSend('sendLocation', function(res) {
             $('#result').html(JSON.stringify(res))
         }, function(err) {
             console.error(err)
