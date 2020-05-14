704a20294cd2b4142f3dce50c6658e9cfdf8e947
Minjae Cho <ussr1285@gmail.com>
2020-05-14 16:42:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,20 @@
 
         // "sendLocation"는 react-native에서 받는 메서드 이름입니다.
         window.webViewBridge.send('sendLocation', option, function(res) {
+            
+            l//et jsonData = 
+            
             let posx = JSON.stringify(res.data.posx);
             let posy = JSON.stringify(res.data.posy);
             
-            let text = '<a href="'+ "http://i-web.kr/skins/blockContent/inc/map_test.php?posx="+ posx +"&posy=" + posy +'">go Map</a>;' 
+            let text = '<a href="'+ "http://i-web.kr/skins/blockContent/inc/map_test.php?posx="+ posx +"&posy=" + posy +'">go Map</a>;'; 
             
-            $('#result').html(res); // JSON.stringify(res)
+            $('#result').html(typeof(res)); // JSON.stringify(res)
             
             $('#mapLink').html(text);
             
         }, function(err) {
-            console.error(err)
+            console.error(err);
         })
     })
 
