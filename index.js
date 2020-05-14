6750683007dce05360897ceac36191a9748e5999
Minjae Cho <ussr1285@gmail.com>
2020-05-14 16:51:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
             
             let text = '<a href="'+ "http://i-web.kr/skins/blockContent/inc/map_test.php?posx="+ posx +"&posy=" + posy +'">go Map</a>;'; 
             
-            $('#result').html( posx ); // JSON.stringify(res)
+            $('#result').html( JSON.stringify(res) ); // JSON.stringify(res)
             
             $('#mapLink').html(text);
             
